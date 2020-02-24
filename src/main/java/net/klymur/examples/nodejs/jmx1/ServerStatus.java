93947d8905201eea99bbdc4b02c25dd6159e5995
Jeff Burke <potatopankakes@gmail.com>
2020-02-24 14:15:00 -0700

    remove unneeded annotations and imports

--- src/main/java/net/klymur/examples/nodejs/jmx1/ServerStatus.java
+++ src/main/java/net/klymur/examples/nodejs/jmx1/ServerStatus.java
@@ -1,9 +1,6 @@
 package net.klymur.examples.nodejs.jmx1;
 
-import org.graalvm.polyglot.HostAccess;
-
 public class ServerStatus implements ServerStatusMBean {
-@HostAccess.Export
   public ServerStatus() {
     nRequestsFailed = 0;
     nRequestsSucceeded = 0;
@@ -12,30 +9,25 @@ public class ServerStatus implements ServerStatusMBean {
   private int nRequestsFailed;
   private int nRequestsSucceeded;
 
-@HostAccess.Export
   @Override
   public int getNumberOfApiRequests() {
     return nRequestsFailed + nRequestsSucceeded;
   }
 
-@HostAccess.Export
   @Override
   public int getNumberOfApiRequestsFailed() {
     return nRequestsFailed;
   }
 
-@HostAccess.Export
   @Override
   public int getNumberOfApiRequestsSucceeded() {
     return nRequestsSucceeded;
   }
 
-@HostAccess.Export
   public void incrementNumberOfApiRequestsFailed() {
     nRequestsFailed++;
   }
 
-@HostAccess.Export
   public void incrementNumberOfApiRequestsSucceeded() {
     nRequestsSucceeded++;
   }
--- src/main/java/net/klymur/examples/nodejs/jmx1/ServerStatusMBean.java
+++ src/main/java/net/klymur/examples/nodejs/jmx1/ServerStatusMBean.java
@@ -1,13 +1,8 @@
 package net.klymur.examples.nodejs.jmx1;
 
-import org.graalvm.polyglot.HostAccess;
-
 public interface ServerStatusMBean {
-@HostAccess.Export
   public int getNumberOfApiRequests();
-@HostAccess.Export
   public int getNumberOfApiRequestsFailed();
-@HostAccess.Export
   public int getNumberOfApiRequestsSucceeded();
 }
 
